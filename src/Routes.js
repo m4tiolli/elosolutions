3598b96242acf1c639cb9cd20088212ec97d2d6c
m4tiolli <gabrielmatiollif@gmail.com>
2023-07-28 16:51:30 -0300

    deploy

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,13 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  Route,
+  RouterProvider,
+  Routes,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom"; // Importe o HashRouter
+// Remova a importação do RouterProvider e createBrowserRouter
 
-//Importação de Páginas
+// Importações das páginas
 import Home from "./pages/Home/Home";
 import Empresa from "./pages/Empresa/Empresa";
 import Contato from "./pages/Contato/Contato";
@@ -10,41 +17,22 @@ import Servicos from "./pages/Servicos/Servicos";
 import Locacao from "./pages/Locacao/Locacao";
 import Assistencia from "./pages/Assistencia/Assistencia";
 
-function Routes() {
-  const router = createBrowserRouter([
-    {
-      path: "",
-      element: <Home />,
-    },
-    {
-      path: "/empresa",
-      element: <Empresa />,
-    },
-    {
-      path: "/contato",
-      element: <Contato />,
-    },
-    {
-      path: "/produtos/inversor",
-      element: <Produtos />,
-    },
-    {
-      path: "/produtos/iot",
-      element: <IOT />,
-    },
-    {
-      path: "/servicos/montagem-painel",
-      element: <Servicos />,
-    },
-    {
-      path: "/servicos/locacao-equipamento",
-      element: <Locacao />,
-    },
-    {
-      path: "/servicos/assistencia-tecnica",
-      element: <Assistencia />,
-    },
-  ]);
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/">
+      <Route index element={<Home />} />
+      <Route path="empresa" element={<Empresa />} />
+      <Route path="contato" element={<Contato />} />
+      <Route path="produtos/inversor" element={<Produtos />} />
+      <Route path="produtos/iot" element={<IOT />} />
+      <Route path="servicos/montagem-painel" element={<Servicos />} />
+      <Route path="servicos/locacao-equipamento" element={<Locacao />} />
+      <Route path="servicos/assistencia-tecnica" element={<Assistencia />} />
+    </Route>
+  )
+);
+function Routess() {
   return <RouterProvider router={router} />;
 }
-export default Routes;
+
+export default Routess;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import Routes from './Routes';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import Routess from "./Routes";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Routes />
+    <Routess />
   </React.StrictMode>
 );
 
