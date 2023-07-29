c9654cbd105130586725e37b47c39b099995f076
m4tiolli <gabrielmatiollif@gmail.com>
2023-07-29 17:00:02 -0300

    hash router

--- src/Routes.js
+++ src/Routes.js
@@ -18,22 +18,29 @@ import Servicos from "./pages/Servicos/Servicos";
 import Locacao from "./pages/Locacao/Locacao";
 import Assistencia from "./pages/Assistencia/Assistencia";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/">
-      <Route index element={<Home />} />
-      <Route path="empresa" element={<Empresa />} />
-      <Route path="contato" element={<Contato />} />
-      <Route path="produtos/inversor" element={<Produtos />} />
-      <Route path="produtos/iot" element={<IOT />} />
-      <Route path="servicos/montagem-painel" element={<Servicos />} />
-      <Route path="servicos/locacao-equipamento" element={<Locacao />} />
-      <Route path="servicos/assistencia-tecnica" element={<Assistencia />} />
-    </Route>
-  )
-);
 function Routess() {
-  return <RouterProvider router={router} />;
+  return (
+    <HashRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/empresa" element={<Empresa />} />
+        <Route exact path="/contato" element={<Contato />} />
+        <Route exact path="/produtos/inversor" element={<Produtos />} />
+        <Route exact path="/produtos/iot" element={<IOT />} />
+        <Route exact path="/servicos/montagem-painel" element={<Servicos />} />
+        <Route
+          exact
+          path="/servicos/locacao-equipamento"
+          element={<Locacao />}
+        />
+        <Route
+          exact
+          path="/servicos/assistencia-tecnica"
+          element={Assistencia}
+        />
+      </Routes>
+    </HashRouter>
+  );
 }
 
 export default Routess;
